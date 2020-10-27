f3c6a11957292aa8c88ffda89b28281ed0bc11d1
Andrae McKenzie <andrae.mckenzie@bp.com>
2020-10-27 21:05:12 +0000

    Add bio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component} from 'react'; 
 
 class App extends Component {
-    render() {
+
+    constructor(){
+        super();
+        this.state = {displayBio: false };
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this); 
+    }
+
+    //doesn't work
+    toggleDisplayBio() {
+        this.setState({displayBio: !this.state.displayBio}); 
+    }
+    
+    render() { 
         return(
             <div>
                 <h1>Hello!</h1>
-                <p>Andrae is the best</p>
-                <p>Another line</p>
+                {  
+                this.state.displayBio ? ( 
+                 <div>
+                    <p>I am a highly adaptable individual</p>
+                    <p>with both academic and practical IT industry skills, capable of working both independently or within a team. </p>
+                    <p> I am looking for a suitable position, which will prove to be both a personal and professional challenge.</p>
+                    <button onClick = {this.displayBio}>Show Less</button>
+                 </div>
+                ) : (
+                    <div>
+                        <button onClick = {this.displayBio}>Read more</button>
+                    </div>
+                    )
+                }
             </div>
         )
     }
