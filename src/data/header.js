86b5f6ef5b0828c77c06067354a2c06ee4cf84b8
Andrae McKenzie <andrae.mckenzie@bp.com>
2020-11-16 22:55:14 +0000

    updated header

---
+++ src/data/header.js
@@ -0,0 +1,25 @@
+
+const HEADER = [
+    {
+       id: '1', 
+       name: 'Home'
+    }, 
+    {
+        id: '2', 
+        name: 'Work Projects'
+    },
+    {
+        id: '3', 
+        name: 'Personal Projects'
+    },
+    {
+        id: '4', 
+        name: 'Blog'
+    },
+    {
+        id: '5', 
+        name: 'Contact Me'
+    }
+]; 
+
+export default HEADER; 
